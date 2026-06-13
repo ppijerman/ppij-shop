b6034b242127dc3200c241dd4761d3cfc42fec3e
Richie Jonathan <richiejonathande@gmail.com>
2026-06-13 14:52:38 +0200

    add Widerrufsbelehrung page and update pages

--- src/app/impressum/page.tsx
+++ src/app/impressum/page.tsx
@@ -10,13 +10,31 @@ export default function ImpressumPage() {
         </h1>
         <div style={{ display: 'flex', flexDirection: 'column', gap: 28, fontFamily: 'var(--font-body)', color: 'var(--ink)', lineHeight: 1.8 }}>
           <Section title="Anbieter">
-            <p>PPI Jerman / Vereinigung indonesischer Studenten e.V.</p>
+            <p>
+              Vereinigung indonesischer Studenten e.V.<br />
+              Connollystr. 3<br />
+              80809 Munchen
+            </p>
           </Section>
-          <Section title="Hinweis">
-            <p>Die aktuellen Kontaktdaten, der Sitz des Vereins, die Registernummer und die Vertretungsverhaltnisse mussen hier vor dem Go-live erganzt und gepruft werden.</p>
+          <Section title="Register">
+            <p>
+              Vereinsregister: VR 2912<br />
+              Registergericht: Amtsgericht Bonn
+            </p>
+          </Section>
+          <Section title="Vertreten durch">
+            <p>
+              Maria Patricia Viannisa (Vorsitzender)<br />
+              Zulfa Nucaini Afifah (Stellvertretender Vorsitzender)<br />
+              Achmad Alwi Harahar (Stellvertretender Vorsitzender)<br />
+              Maria Bernadine Aurelia (Stellvertretender Vorsitzender)
+            </p>
           </Section>
           <Section title="Kontakt">
-            <p>Bitte wenden Sie sich fur Fragen zum Web Shop an die offiziellen Kontaktkanale von PPI Jerman.</p>
+            <p>E-Mail: partnership@ppijerman.org</p>
+          </Section>
+          <Section title="Verbraucherstreitbeilegung / Universalschlichtungsstelle">
+            <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
           </Section>
         </div>
       </div>
