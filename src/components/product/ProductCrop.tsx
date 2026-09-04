b7a3312dd43a586d90d689a79612886b40c23be2
chika <jessica.felicia@alumni.h-aachen.de>
2026-09-04 19:19:12 +0200

    restored merch image

--- src/components/product/ProductCrop.tsx
+++ src/components/product/ProductCrop.tsx
@@ -10,7 +10,7 @@ export default function ProductCrop({ src, height = 'auto' }: ProductCropProps) 
       width: '100%', 
       height: height === 'auto' ? '100%' : height,
       backgroundImage: `url(${src})`,
-      backgroundSize: '100%',
+      backgroundSize: 'cover',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
       backgroundColor: 'var(--cream-2)',
